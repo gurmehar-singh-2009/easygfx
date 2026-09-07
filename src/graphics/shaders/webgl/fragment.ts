bb791200a1368d8b6153e6e0421cd0c573666673
Umar Ahmed <umarahmed02009@gmail.com>
2026-09-07 18:25:34 -0400

    Update PBR fragment shader.

--- src/graphics/shaders/webgl/fragment.ts
+++ src/graphics/shaders/webgl/fragment.ts
@@ -1,3 +1,5 @@
+// See: https://learnopengl.com/PBR/Lighting
+
 export const fragmentShaderSource = `#version 300 es
 precision mediump float;
 
@@ -17,59 +19,47 @@ out vec4 fragColor;
 
 const float PI = 3.14159265359;
 
-float DistributionGGX(vec3 N, vec3 H, float a) {
-    a = a * a;
-    float a2 = a * a;
-    float NdotH = max(dot(N, H), 0.0);
-    float NdotH2 = NdotH * NdotH;
+vec3 fresnelSchlick(float cosTheta, vec3 F0)
+{
+    return F0 + (1.0 - F0) * pow(clamp(1.0 - cosTheta, 0.0, 1.0), 5.0);
+}
+
+float DistributionGGX(vec3 N, vec3 H, float roughness)
+{
+    float a      = roughness*roughness;
+    float a2     = a*a;
+    float NdotH  = max(dot(N, H), 0.0);
+    float NdotH2 = NdotH*NdotH;
 	
-    float nom = a2;
+    float num   = a2;
     float denom = (NdotH2 * (a2 - 1.0) + 1.0);
     denom = PI * denom * denom;
-
-    return nom / denom;
+	
+    return num / denom;
 }
 
-float GeometrySchlickGGX(float NdotV, float k) {
-    float nom = NdotV;
+float GeometrySchlickGGX(float NdotV, float roughness)
+{
+    float r = (roughness + 1.0);
+    float k = (r*r) / 8.0;
+
+    float num   = NdotV;
     float denom = NdotV * (1.0 - k) + k;
-
-    return nom / denom;
+	
+    return num / denom;
 }
-  
-float GeometrySmith(vec3 N, vec3 V, vec3 L, float k) {
+
+float GeometrySmith(vec3 N, vec3 V, vec3 L, float roughness)
+{
     float NdotV = max(dot(N, V), 0.0);
     float NdotL = max(dot(N, L), 0.0);
-    float ggx1 = GeometrySchlickGGX(NdotV, k);
-    float ggx2 = GeometrySchlickGGX(NdotL, k);
-
+    float ggx2  = GeometrySchlickGGX(NdotV, roughness);
+    float ggx1  = GeometrySchlickGGX(NdotL, roughness);
+	
     return ggx1 * ggx2;
 }
 
-vec3 fresnelSchlick(float cosTheta, vec3 F0) {
-    return F0 + (1.0 - F0) * pow(1.0 - cosTheta, 5.0);
-}
-
-vec3 cookTorrenceBRDF(vec3 lightVector, vec3 viewVector, vec3 halfVector, vec3 surfaceNormal, float roughness, vec3 baseReflectivity) {
-    float cosTheta = max(dot(halfVector, viewVector), 0.0);
-
-    float D = DistributionGGX(surfaceNormal, halfVector, roughness);
-    vec3  F = fresnelSchlick(cosTheta, baseReflectivity);
-
-    float temp = roughness + 1.0;
-    float k = (temp * temp) / 8.0;
-    float G = GeometrySmith(surfaceNormal, viewVector, lightVector, k);
-
-    float NdotL = max(dot(surfaceNormal, lightVector), 0.0);
-    float NdotV = max(dot(surfaceNormal, viewVector), 0.0);
-
-    vec3 numerator = D * F * G;
-    float denominator = 4.0 * NdotL * NdotV + 0.0001;
-
-    return numerator / denominator;
-}
-
-vec3 acesToneMapping(vec3 x) {
+vec3 aces(vec3 x) {
     float a = 2.51;
     float b = 0.03;
     float c = 2.43;
@@ -78,16 +68,22 @@ vec3 acesToneMapping(vec3 x) {
     return clamp((x * (a * x + b)) / (x * (c * x + d) + e), 0.0, 1.0);
 }
 
-void main() { 
+void main() {
+    vec4 surfaceColor = u_albedo;
+    if (u_textureIds.x >= 0) {
+        vec4 textureColor = texture(u_textures, vec3(v_texCoord, float(u_textureIds.x)));
+        surfaceColor *= textureColor;
+    }
+
     if (u_pbrProperties.w > 0.5) {
-        vec4 surfaceColor = u_albedo;
-        if (u_textureIds.x >= 0) {
-            vec4 textureColor = texture(u_textures, vec3(v_texCoord, float(u_textureIds.x)));
-            surfaceColor *= textureColor;
-        }
         fragColor = surfaceColor;
         return;
     }
+
+    surfaceColor.rgb = pow(surfaceColor.rgb, vec3(2.2));
+
+    vec3 lightPosition = vec3(0.0);
+    vec3 lightColor = vec3(100.0, 100.0, 110.0);
 
     vec3 N = normalize(v_normal);
     vec3 V = normalize(u_cameraPosition - v_worldPosition);
@@ -101,14 +97,7 @@ void main() {
         N = normalize(TBN * mapNormal);
     }
 
-    vec4 surfaceColor = u_albedo;
-    if (u_textureIds.x >= 0) {
-        vec4 textureColor = texture(u_textures, vec3(v_texCoord, float(u_textureIds.x)));
-        surfaceColor *= textureColor;
-    }
-    vec3 albedoLinear = pow(surfaceColor.rgb, vec3(2.2));
-
-    float roughness = clamp(u_pbrProperties.x, 0.05, 1.0);
+    float roughness = clamp(u_pbrProperties.x, 0.0001, 1.0);
     float metallic = clamp(u_pbrProperties.y, 0.0, 1.0);
     float ao = u_pbrProperties.z;
 
@@ -117,30 +106,41 @@ void main() {
         ao *= orm.r;
         roughness *= orm.g;
         metallic *= orm.b;
-        roughness = clamp(roughness, 0.05, 1.0);
+        roughness = clamp(roughness, 0.0001, 1.0);
     }
 
-    vec3 lightColor = vec3(10.0, 10.0, 10.0);
-    vec3 lightVector = normalize(vec3(0.0, 0.0, 1.0));
-    vec3 halfVector = normalize(lightVector + V);
+    vec3 F0 = vec3(0.04);
+    F0 = mix(F0, surfaceColor.rgb, metallic);
 
-    vec3 baseReflectivity = mix(vec3(0.04), albedoLinear, metallic);
+    vec3 Lo = vec3(0.0);
+    
+    vec3 diff = lightPosition - v_worldPosition;
+    vec3 L = normalize(diff);
+    vec3 H = normalize(V + L);
+    float distance = length(diff);
+    float attenuation = 1.0 / (distance * distance);
+    vec3 radiance = lightColor * attenuation;
 
-    vec3 specularBRDF = cookTorrenceBRDF(lightVector, V, halfVector, N, roughness, baseReflectivity);
+    float NDF = DistributionGGX(N, H, roughness);
+    float G = GeometrySmith(N, V, L, roughness);
+    vec3 F = fresnelSchlick(max(dot(H, V), 0.0), F0);
+    
+    vec3 kS = F;
+    vec3 kD = vec3(1.0) - kS;
+    kD *= 1.0 - metallic;  
+        
+    vec3 numerator = NDF * G * F;
+    float denominator = 4.0 * max(dot(N, V), 0.0) * max(dot(N, L), 0.0) + 0.0001;
+    vec3 specular = numerator / denominator;
 
-    float HdotV = max(dot(halfVector, V), 0.0);
-    vec3 kS = fresnelSchlick(HdotV, baseReflectivity);
-    vec3 kD = (vec3(1.0) - kS) * (1.0 - metallic);
+    float NdotL = max(dot(N, L), 0.0);
+    Lo += (kD * surfaceColor.rgb / PI + specular) * radiance * NdotL;
+  
+    vec3 ambient = vec3(0.03) * surfaceColor.rgb * ao;
+    vec3 color = ambient + Lo;
 
-    float NdotL = max(dot(N, lightVector), 0.0);
-    vec3 diffuseBRDF = kD * (albedoLinear / PI);
-    vec3 outgoing = (diffuseBRDF + specularBRDF) * lightColor * NdotL;
-
-    vec3 ambient = vec3(0.03) * albedoLinear * ao;
-    vec3 color = ambient + outgoing;
-
-    color = acesToneMapping(color);
+    color = aces(color);
     color = pow(color, vec3(1.0 / 2.2));
 
     fragColor = vec4(color, surfaceColor.a);
-}`;
+} `;
